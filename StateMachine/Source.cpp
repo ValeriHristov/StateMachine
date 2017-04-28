2ca5a034da13d3bb76c9d2550f59a89729884300
ValeriHristov <valeri.dhristov@gmail.com>
2017-04-29 00:40:24 +0300

    Remove old tests

--- StateMachine/Source.cpp
+++ StateMachine/Source.cpp
@@ -36,19 +36,5 @@ int main()
 	StateMachine fourth = third.Union(StateMachine('g'));
 	std::cout << fourth.Recognize("t");*/
 
-
-	//std::unordered_map<char, std::vector<Transition>> a;
-	//a['a'].push_back(Transition());
-	//std::unordered_map<char, std::vector<Transition>> b = a;
-
-	//b['a'].pop_back();
-	//std::cout << a['a'].capacity();
-	////std::vector<Transition> t;
-	////t.push_back(Transition());
-	////std::vector<Transition> c = t;
-	////c.pop_back();
-	////std::cout << t.capacity();
-	/*StateMachine();
-	*/
 	return 0;
 }
