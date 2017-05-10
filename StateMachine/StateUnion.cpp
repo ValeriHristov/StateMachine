4e68c6245c68c52a13dd00863cf9ef44a9096ec2
ValeriHristov <valeri.dhristov@gmail.com>
2017-05-10 19:17:48 +0300

    Removed unused method

--- StateMachine/StateUnion.cpp
+++ StateMachine/StateUnion.cpp
@@ -5,12 +5,6 @@ bool StateUnion::operator==(const StateUnion& other) const
 	return this->statesIndexes == other.statesIndexes;
 }
 
-void StateUnion::AddFunctionality(const State& other, bool copyIsFinal,int index)
-{
-	State::AddFunctionality(other, copyIsFinal);
-	this->statesIndexes.AddElement(index);
-}
-
 StateUnion::StateUnion()
 {}
 
--- StateMachine/StateUnion.h
+++ StateMachine/StateUnion.h
@@ -8,7 +8,6 @@ public:
 	StateUnion();
 	StateUnion(bool isFinal);
 	bool operator==(const StateUnion& other) const;
-	void AddFunctionality(const State&, bool copyIsFinal, int index);
 	void AddStateIndex(int index);
 	Sorted<int> GetStatesIndexes()const
 	{
